6c0dbdf5aad6b1c800fc46cbdda2865a5d6cadbe
DagimDev <simedagim121@gmail.com>
2025-07-04 15:40:12 +0300

    Implement random snake coordinates generation

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,21 @@ const ctx = canvas.getContext("2d");
 
 console.log(ctx);
 
-const snake = [];
-snake[0] = {
-  x: 0,
-  y: 0,
-};
-
-console.log(snake);
-const scale = 20;
+const scale = 10;
 const row = canvas.height / scale;
 const column = canvas.width / scale;
 // console.log(`Row: ${row}`)
 // console.log(`Column: ${column}`)
 
+const snake = [];
+snake[0] = {
+  x: Math.floor(Math.random() * row) * scale,
+  y: Math.floor(Math.random() * column) * scale,
+};
+
+console.log(snake[0].x);
+console.log(snake[0].y);
+
 ctx.strokeStyle = "red";
 ctx.fillStyle = "#fff";
-ctx.fillRect(snake[0].x, snake[0].y, 20, 20);
+ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
