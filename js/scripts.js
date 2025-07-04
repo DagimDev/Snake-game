be9de31aa4b603e9c08206b7a215c539dac67b68
DagimDev <simedagim121@gmail.com>
2025-07-04 16:29:16 +0300

    Implement snake movement with directional controls

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,39 @@ snake[0] = {
 console.log(snake[0].x);
 console.log(snake[0].y);
 
-ctx.strokeStyle = "red";
-ctx.fillStyle = "#fff";
-ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
+let direction = "right";
+// let playGame = setInterval(draw, 100);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "red";
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
+
+//   if(snake[0].x > 480)  {
+//     return
+//   }
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  console.log(`X: ${snakeX}`)
+  console.log(`Y: ${snakeY}`)
+
+  if (direction === "right") snakeX += scale
+  if (direction === "up") snakeY -= scale
+  if (direction === "left") snakeX -= scale
+  if (direction === "down") snakeY += scale
+
+  console.log(`next X: ${snakeX}`)
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+//   snake.pop()
+  snake.unshift(newHead)
+
+}
