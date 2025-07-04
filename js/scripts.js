01c73397c3a51f14f30d840faa678dacc81601ef
DagimDev <simedagim121@gmail.com>
2025-07-04 21:22:17 +0300

    refactor: code

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ console.log(snake[0].x);
 console.log(snake[0].y);
 
 let direction = "right";
-// let playGame = setInterval(draw, 100);
+let playGame = setInterval(draw, 100);
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -28,29 +28,28 @@ function draw() {
   ctx.fillStyle = "#fff";
   ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
 
-//   if(snake[0].x > 480)  {
-//     return
-//   }
+  //   if(snake[0].x > 480)  {
+  //     return
+  //   }
 
-  let snakeX = snake[0].x
-  let snakeY = snake[0].y
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
 
-  console.log(`X: ${snakeX}`)
-  console.log(`Y: ${snakeY}`)
+  console.log(`X: ${snakeX}`);
+  console.log(`Y: ${snakeY}`);
 
-  if (direction === "right") snakeX += scale
-  if (direction === "up") snakeY -= scale
-  if (direction === "left") snakeX -= scale
-  if (direction === "down") snakeY += scale
+  if (direction === "right") snakeX += scale;
+  if (direction === "up") snakeY -= scale;
+  if (direction === "left") snakeX -= scale;
+  if (direction === "down") snakeY += scale;
 
-  console.log(`next X: ${snakeX}`)
+  console.log(`next X: ${snakeX}`);
 
   let newHead = {
     x: snakeX,
-    y: snakeY
-  }
+    y: snakeY,
+  };
 
-//   snake.pop()
-  snake.unshift(newHead)
-
+  //   snake.pop()
+  snake.unshift(newHead);
 }
